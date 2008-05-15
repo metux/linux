414db31c111b0e87db209855b9f3d2979aa9a34d
San Mehat <san@android.com>
2008-05-15 09:15:37 -0700

    mmc: Add new API call 'sdio_reset_comm' for resetting communication with an SDIO device
    mmc: sdio: Disable retuning in sdio_reset_comm()
    
    Since sdio_reset_comm() re-initializes the SDIO card, disable retuning
    before idling and shutting down the card.  Tuning will be re-enabled (if
    necessary) in mmc_sdio_init_card().
    
    BUG=chrome-os-partner:46444
    TEST=With CL:311815, toggle WiFi on/off on Smaug and observe that the
    WiFi card comes back up and is able to tune successfully.
    
    Change-Id: Ib4a5cfd4d75fc9e3ed7bb3f1e2ffd30de16c5d28
    Signed-off-by: Andrew Bresticker <abrestic@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/311797
    Reviewed-by: Derek Basehore <dbasehore@chromium.org>
    
    [briannorris: brought from Chromium kernel in 3.18 -> 4.4 rebase]
    
    Signed-off-by: Brian Norris <briannorris@google.com>

--- drivers/mmc/core/sdio.c
+++ drivers/mmc/core/sdio.c
@@ -10,6 +10,7 @@
  */
 
 #include <linux/err.h>
+#include <linux/module.h>
 #include <linux/pm_runtime.h>
 
 #include <linux/mmc/host.h>
@@ -21,6 +22,7 @@
 
 #include "core.h"
 #include "bus.h"
+#include "host.h"
 #include "sd.h"
 #include "sdio_bus.h"
 #include "mmc_ops.h"
@@ -1215,3 +1217,42 @@ err:
 	return err;
 }
 
+int sdio_reset_comm(struct mmc_card *card)
+{
+	struct mmc_host *host = card->host;
+	u32 ocr;
+	u32 rocr;
+	int err;
+
+	printk("%s():\n", __func__);
+	mmc_claim_host(host);
+
+	mmc_retune_disable(host);
+
+	mmc_go_idle(host);
+
+	mmc_set_clock(host, host->f_min);
+
+	err = mmc_send_io_op_cond(host, 0, &ocr);
+	if (err)
+		goto err;
+
+	rocr = mmc_select_voltage(host, ocr);
+	if (!rocr) {
+		err = -EINVAL;
+		goto err;
+	}
+
+	err = mmc_sdio_init_card(host, rocr, card, 0);
+	if (err)
+		goto err;
+
+	mmc_release_host(host);
+	return 0;
+err:
+	printk("%s: Error resetting SDIO communications (%d)\n",
+	       mmc_hostname(host), err);
+	mmc_release_host(host);
+	return err;
+}
+EXPORT_SYMBOL(sdio_reset_comm);

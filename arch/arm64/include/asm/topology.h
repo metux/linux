baeaf95826d9737604b9880c1389c291a4d8e9b3
Juri Lelli <juri.lelli@arm.com>
2015-04-30 11:53:48 +0100

    arm64: Cpu invariant scheduler load-tracking and capacity support
    Provides the scheduler with a cpu scaling correction factor for more
    accurate load-tracking and cpu capacity handling.
    
    The Energy Model (EM) (in fact the capacity value of the last element
    of the capacity states vector of the core (MC) level sched_group_energy
    structure) is used as the source for this cpu scaling factor.
    
    The cpu capacity value depends on the micro-architecture and the
    maximum frequency of the cpu.
    
    The maximum frequency part should not be confused with the frequency
    invariant scheduler load-tracking support which deals with frequency
    related scaling due to DFVS functionality.
    
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>

--- arch/arm64/include/asm/topology.h
+++ arch/arm64/include/asm/topology.h
@@ -32,11 +32,13 @@ int pcibus_to_node(struct pci_bus *bus);
 
 #endif /* CONFIG_NUMA */
 
+struct sched_domain;
 #ifdef CONFIG_CPU_FREQ
 #define arch_scale_freq_capacity cpufreq_scale_freq_capacity
-struct sched_domain;
 extern unsigned long cpufreq_scale_freq_capacity(struct sched_domain *sd, int cpu);
 #endif
+#define arch_scale_cpu_capacity scale_cpu_capacity
+extern unsigned long scale_cpu_capacity(struct sched_domain *sd, int cpu);
 
 #include <asm-generic/topology.h>
 

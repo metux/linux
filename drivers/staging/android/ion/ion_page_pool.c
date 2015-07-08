fc25512ee8fd4d96acb7f1fb7d69acc7a2c999e4
Vinil Cheeramvelil <vinil.cheeramvelil@intel.com>
2015-07-08 10:35:06 +0800

    ion: Handle the memory mapping correctly on x86
    This patch modifies the ion page pool code to address
    limitation in x86 PAT. When one physical page is mapped
    to multiple virtual pages, the same cache policy
    should be used. Add set_memory_wc/uc call to avoid aliases.
    If not, all mappings will be cached(write back).
    
    Change-Id: I98ee8902df0c80135dddfa998c4ca4c2bb44e40e
    Signed-off-by: Zhebin Jin <zhebin.jin@intel.com>
    Signed-off-by: Vinil Cheeramvelil <vinil.cheeramvelil@intel.com>

--- drivers/staging/android/ion/ion_page_pool.c
+++ drivers/staging/android/ion/ion_page_pool.c
@@ -30,6 +30,9 @@ static void *ion_page_pool_alloc_pages(struct ion_page_pool *pool)
 
 	if (!page)
 		return NULL;
+
+	ion_page_pool_alloc_set_cache_policy(pool, page);
+
 	if (!pool->cached)
 		ion_pages_sync_for_device(NULL, page, PAGE_SIZE << pool->order,
 					  DMA_BIDIRECTIONAL);
@@ -39,6 +42,7 @@ static void *ion_page_pool_alloc_pages(struct ion_page_pool *pool)
 static void ion_page_pool_free_pages(struct ion_page_pool *pool,
 				     struct page *page)
 {
+	ion_page_pool_free_set_cache_policy(pool, page);
 	__free_pages(page, pool->order);
 }
 
@@ -102,6 +106,11 @@ void ion_page_pool_free(struct ion_page_pool *pool, struct page *page)
 	ret = ion_page_pool_add(pool, page);
 	if (ret)
 		ion_page_pool_free_pages(pool, page);
+}
+
+void ion_page_pool_free_immediate(struct ion_page_pool *pool, struct page *page)
+{
+	ion_page_pool_free_pages(pool, page);
 }
 
 static int ion_page_pool_total(struct ion_page_pool *pool, bool high)

8d0e8a5a6519029885c56c042ef8b14b0ce16664
Morten Rasmussen <morten.rasmussen@arm.com>
2016-02-25 12:47:54 +0000

    sched: Add group_misfit_task load-balance type
    To maximize throughput in systems with reduced capacity cpus (e.g.
    high RT/IRQ load and/or ARM big.LITTLE) load-balancing has to consider
    task and cpu utilization as well as per-cpu compute capacity when
    load-balancing in addition to the current average load based
    load-balancing policy. Tasks that are scheduled on a reduced capacity
    cpu need to be identified and migrated to a higher capacity cpu if
    possible.
    
    To implement this additional policy an additional group_type
    (load-balance scenario) is added: group_misfit_task. This represents
    scenarios where a sched_group has tasks that are not suitable for its
    per-cpu capacity. group_misfit_task is only considered if the system is
    not overloaded in any other way (group_imbalanced or group_overloaded).
    
    Identifying misfit tasks requires the rq lock to be held. To avoid
    taking remote rq locks to examine source sched_groups for misfit tasks,
    each cpu is responsible for tracking misfit tasks themselves and update
    the rq->misfit_task flag. This means checking task utilization when
    tasks are scheduled and on sched_tick.
    
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -612,6 +612,8 @@ struct rq {
 #endif
 	#define CPU_LOAD_IDX_MAX 5
 	unsigned long cpu_load[CPU_LOAD_IDX_MAX];
+	unsigned long last_load_update_tick;
+	unsigned int misfit_task;
 #ifdef CONFIG_NO_HZ_COMMON
 #ifdef CONFIG_SMP
 	unsigned long last_load_update_tick;

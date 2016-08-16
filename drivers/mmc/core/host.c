dd4f524e2ce7f4ccf401810cd727b75ebb42eae2
Mohan Srinivasan <srmohan@google.com>
2016-08-16 15:51:34 -0700

    Android: MMC/UFS IO Latency Histograms.
    This patch adds a new sysfs node (latency_hist) and reports IO
    (svc time) latency histograms. Disabled by default, can be enabled
    by echoing 0 into latency_hist, stats can be cleared by writing 2
    into latency_hist.
    
    Bug: 30677035
    Change-Id: I625938135ea33e6e87cf6af1fc7edc136d8b4b32
    Signed-off-by: Mohan Srinivasan <srmohan@google.com>

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -31,9 +31,7 @@
 #include "slot-gpio.h"
 #include "pwrseq.h"
 
-#define cls_dev_to_mmc_host(d)	container_of(d, struct mmc_host, class_dev)
-
-static DEFINE_IDA(mmc_host_ida);
+static DEFINE_IDR(mmc_host_idr);
 static DEFINE_SPINLOCK(mmc_host_lock);
 
 static void mmc_host_classdev_release(struct device *dev)
@@ -430,6 +428,10 @@ int mmc_add_host(struct mmc_host *host)
 	mmc_add_host_debugfs(host);
 #endif
 
+#ifdef CONFIG_BLOCK
+	mmc_latency_hist_sysfs_init(host);
+#endif
+
 	mmc_start_host(host);
 	if (!(host->pm_flags & MMC_PM_IGNORE_PM_NOTIFY))
 		register_pm_notifier(&host->pm_notify);
@@ -456,6 +458,10 @@ void mmc_remove_host(struct mmc_host *host)
 
 #ifdef CONFIG_DEBUG_FS
 	mmc_remove_host_debugfs(host);
+#endif
+
+#ifdef CONFIG_BLOCK
+	mmc_latency_hist_sysfs_exit(host);
 #endif
 
 	device_del(&host->class_dev);

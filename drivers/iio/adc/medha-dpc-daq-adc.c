674ad7499cdba0ba1589527012858c377f673fd3
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2017-11-02 14:05:22 +0000

    1

--- drivers/iio/adc/medha-dpc-daq-adc.c
+++ drivers/iio/adc/medha-dpc-daq-adc.c
@@ -25,75 +25,23 @@
 #include <linux/of_gpio.h>
 #include <linux/of_platform.h>
 #include <linux/regmap.h>
-
+#include <linux/iio/iio.h>
+#include <linux/iio/triggered_buffer.h>
 
 #define MAX_CHAN	3
 
 struct medha_dpc_daq_adc_channel {
 	int id;
 	int addr;
-	int irq;
+	int irq_a;
+	int irq_b;
 };
 
-struct medha_dpc_daq_adc_device {
+struct adc_device {
 	struct completion			done;
-	spinlock_t				spi_lock;		// FIXME: init
 	struct medha_dpc_daq_adc_channel	chan[MAX_CHAN];
-	struct spi_device			*spi;
+	struct platform_device			*pdev;
 };
-
-static int medha_dpc_daq_adc_spicall(struct medha_dpc_daq_adc_device *dev, struct spi_message *msg)
-{
-	unsigned long lockflags = 0;
-	struct spi_device *spidev = to_spi_device(dev);
-
-	spin_lock_irqsave(&dev->spi_lock, lockflags);
-	spi_sync(spidev, msg);
-	spin_unlock_irqrestore(&dev->spi_lock, lockflags);
-
-	return 0;
-}
-
-static u16 medha_dpc_daq_adc_read16(struct medha_dpc_daq_adc_device *dev, u8 reg)
-{
-	u16 val = 0;
-
-	struct spi_transfer tr0 = { .tx_buf = &reg, .len = sizeof(reg) };
-	struct spi_transfer tr1 = { .rx_buf = &val, .len = sizeof(val) };
-
-	struct spi_message msg;
-	spi_message_init(&msg);
-	spi_message_add_tail(&t0, &msg);
-	spi_message_add_tail(&t1, &msg);
-
-	return medha_dpc_daq_adc_spicall(dev, &msg);
-}
-
-static int medha_dpc_daq_adc_write8(struct medha_dpc_daq_adc_device *dev, u8 reg, u8 val)
-{
-	struct spi_transfer tr0 = { .tx_buf = &reg, .len = sizeof(reg) };
-	struct spi_transfer tr1 = { .tx_buf = &val, .len = sizeof(val) };
-
-	struct spi_message msg;
-	spi_message_init(&msg);
-	spi_message_add_tail(&t0, &msg);
-	spi_message_add_tail(&t1, &msg);
-
-	return medha_dpc_daq_adc_spicall(dev, &msg);
-}
-
-static int medha_dpc_daq_adc_write16(struct medha_dpc_daq_adc_device *dev, u8 reg, u16 val)
-{
-	struct spi_transfer tr0 = { .tx_buf = &reg, .len = sizeof(reg) };
-	struct spi_transfer tr1 = { .tx_buf = &val, .len = sizeof(val) };
-
-	struct spi_message msg;
-	spi_message_init(&msg);
-	spi_message_add_tail(&t0, &msg);
-	spi_message_add_tail(&t1, &msg);
-
-	return medha_dpc_daq_adc_spicall(dev, &msg);
-}
 
 irqreturn_t trigger_handler(int irq, void *dev_id)
 {
@@ -102,31 +50,136 @@ irqreturn_t trigger_handler(int irq, void *dev_id)
 	return IRQ_HANDLED;
 }
 
-static int medha_dpc_daq_adc_probe(struct spi_device *spi)
+static int adc_read_raw(struct iio_dev *idev,
+			struct iio_chan_spec const *chan,
+			int *val, int *val2, long mask)
+{
+/*	struct adc_device *st = iio_priv(idev);
+	int ret; */
+
+	*val = 23;
+	*val2 = 66;
+
+	printk(KERN_INFO "adc_read_raw\n");
+	switch (mask) {
+		case IIO_CHAN_INFO_RAW:
+			printk(KERN_INFO " == IIO_CHAN_INFO_RAW\n");
+			return IIO_VAL_INT;
+		break;
+		case IIO_CHAN_INFO_SCALE:
+			printk(KERN_INFO " == IIO_CHAN_INFO_SCALE\n");
+			return IIO_VAL_FRACTIONAL_LOG2;
+		break;
+		default:
+			printk(KERN_INFO " == default\n");
+		break;
+	}
+
+	return -EINVAL;
+}
+
+static const struct iio_info adc_info = {
+	.driver_module = THIS_MODULE,
+	.read_raw = &adc_read_raw,
+};
+
+// BIT(IIO_CHAN_INFO_RAW)|BIT(IIO_CHAN_INFO_SCALE)
+static const struct iio_chan_spec adc_channels[3] = {
+	{
+		.type				= IIO_VOLTAGE,
+		.info_mask_separate		= BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_type	= BIT(IIO_CHAN_INFO_SCALE),
+		.indexed			= 1,
+		.channel			= 0,
+		.scan_index			= 0,
+		.scan_type			= {
+			.sign		= 'u',
+			.realbits	= 24,
+			.storagebits	= 32,
+		},
+	},
+	{
+		.type				= IIO_VOLTAGE,
+		.info_mask_separate		= BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_type	= BIT(IIO_CHAN_INFO_SCALE),
+		.indexed			= 1,
+		.channel			= 1,
+		.scan_index			= 1,
+		.scan_type			= {
+			.sign		= 'u',
+			.realbits	= 24,
+			.storagebits	= 32,
+		},
+	},
+	{
+		.type				= IIO_VOLTAGE,
+		.info_mask_separate		= BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_type	= BIT(IIO_CHAN_INFO_SCALE),
+		.indexed			= 1,
+		.channel			= 2,
+		.scan_index			= 3,
+		.scan_type			= {
+			.sign		= 'u',
+			.realbits	= 24,
+			.storagebits	= 32,
+		},
+	},
+};
+
+static int adc_driver_probe(struct platform_device *pdev)
 {
 	int ret = 0;
 	struct iio_dev *iiodev;
-	struct medha_dpc_daq_adc_device *priv;
+	struct adc_device *priv;
+	struct resource *res;
+	void __iomem *base;
 
-	iiodev = devm_iio_device_alloc(&spi->dev, sizeof(*priv));
-	if (!iiodev)
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	base = devm_ioremap_resource(&pdev->dev, res);
+	if (IS_ERR(base)) {
+		dev_err(&pdev->dev, "adc_driver_probe() failed to get iomem\n");
+		return PTR_ERR(base);
+	}
+
+	iiodev = devm_iio_device_alloc(&pdev->dev, sizeof(*priv));
+	if (!iiodev) {
+		dev_err(&pdev->dev, "failed to allocate iio device\n");
 		return -ENOMEM;
+	}
+
+	iiodev->info = &adc_info;
+
+	platform_set_drvdata(pdev, iiodev);
 
 	priv = iio_priv(iiodev);
-	spi_set_drvdata(spi, iio_dev);
+	priv->pdev = pdev;
+	priv->chan[0].irq_a = platform_get_irq_byname(pdev, "adc0-0");
+	priv->chan[0].irq_b = platform_get_irq_byname(pdev, "adc0-1");
+	priv->chan[1].irq_a = platform_get_irq_byname(pdev, "adc1-0");
+	priv->chan[1].irq_b = platform_get_irq_byname(pdev, "adc1-1");
+	priv->chan[2].irq_a = platform_get_irq_byname(pdev, "adc2-0");
+	priv->chan[2].irq_b = platform_get_irq_byname(pdev, "adc2-1");
 
-	priv->spi = spi;
+	dev_info(&pdev->dev, "IRQs: %d %d %d %d %d %d\n",
+		priv->chan[0].irq_a,
+		priv->chan[0].irq_b,
+		priv->chan[1].irq_a,
+		priv->chan[1].irq_b,
+		priv->chan[2].irq_a,
+		priv->chan[2].irq_b);
 
-	iiodev->dev.parent = &spi->dev;
-	iiodev->dev.of_node = spi->dev.of_node;
-	iiodev->name = spi_get_device_id(spi)->name;
-	iiodev->modes = INDIO_DIRECT_MODE;
-	iiodev->channels = MAX_CHAN;
+	iiodev->dev.parent = &pdev->dev;
+	iiodev->dev.of_node = pdev->dev.of_node;
+	iiodev->name = platform_get_device_id(pdev)->name;
+	iiodev->modes = INDIO_DIRECT_MODE /*INDIO_BUFFER_HARDWARE*/;
+	iiodev->channels = adc_channels;
+	iiodev->num_channels = ARRAY_SIZE(adc_channels);
+	iiodev->info = &adc_info;
 
-	if (ret = iio_triggered_buffer_setup(iiodev, NULL, &trigger_handler, NULL))
+	if ((ret = iio_triggered_buffer_setup(iiodev, NULL, &trigger_handler, NULL)))
 		goto err_buffer;
 
-	if (ret = iio_device_register(iiodev))
+	if ((ret = iio_device_register(iiodev)))
 		goto err_unregister;
 
 	return 0;
@@ -137,31 +190,18 @@ err_buffer:
 	return ret;
 }
 
-static int medha_dpc_daq_adc_remove(struct spi_device *spi)
-{
-	struct iio_dev *iiodev = spi_get_drvdata(spi);
-	struct medha_dpc_daq_adc_device *priv;
-
-	iio_device_unregister(iiodev);
-	iio_triggered_buffer_cleanup(iiodev);
-
-	return 0;
-}
-
-static const struct of_device_id medha_dpc_daq_adc_dt_match[] = {
-	{ .compatible = "medha,dpc-daq-adc" },
+static const struct of_device_id adc_dt_match[] = {
+	{ .compatible = "medha,m337decc-02-adc" },
 	{ }
 };
 
-static struct spi_driver medha_dpc_daq_adc_driver = {
-	.probe		= medha_dpc_daq_adc_probe,
-	.remove		= medha_dpc_daq_adc_probe,
+static struct platform_driver adc_driver = {
 	.driver		= {
-		.name   = "medha-dpc-daq-adc",
-		.of_match_table = of_match_ptr(medha_dpc_daq_adc_dt_match),
+		.name   = "medha,m337decc-02-adc",
+		.of_match_table = of_match_ptr(adc_dt_match),
 	},
 };
-module_spi_driver(medha_dpc_daq_adc_driver);
+module_platform_driver_probe(adc_driver, adc_driver_probe);
 
 MODULE_AUTHOR("Enrico Weigelt, metux IT consule <info@metux.net>");
 MODULE_DESCRIPTION("ADC driver for Medha Railway DPC DAQ/diagnostics module");

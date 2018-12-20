3b14403d1b0a154ac2c945cc743f3c1c48576be4
Enrico Weigelt, metux IT consult <info@metux.net>
2018-12-20 17:50:05 +0100

    (submitted) input: keyboard: gpio-keys-polled: use input name from pdata if available
    Instead of hardcoding the input name to the driver name
    ('gpio-keys-polled'), allow the passing a name via platform data
    ('name' field was already present), but default to old behaviour
    in case of NULL.
    
    Even though the general tendency is moving from pdata structs,
    towards oftree/acpi/fwnode, pdata structs still have their valid
    use cases, when the mentioned mechanisms aren't available or don't
    provide the necessary data and so driver setup still needs to be
    done explicitly.
    
    An example use case is keys+led support for the APUv2/3 boards.
    Here a board specific platform driver probes the board and then
    instantiates the individual devices using classic pdata.
    
    The gpio-keys-polled currently lacks support for specifying
    input device name, which is fixed by this patch.
    
    Cc: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Cc: linux-input@vger.kernel.org
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
    Submitted: 2019-06-07

--- drivers/input/keyboard/gpio_keys_polled.c
+++ drivers/input/keyboard/gpio_keys_polled.c
@@ -272,7 +272,7 @@ static int gpio_keys_polled_probe(struct platform_device *pdev)
 
 	input = poll_dev->input;
 
-	input->name = pdev->name;
+	input->name = (pdata->name ? pdata->name : pdev->name);
 	input->phys = DRV_NAME"/input0";
 
 	input->id.bustype = BUS_HOST;

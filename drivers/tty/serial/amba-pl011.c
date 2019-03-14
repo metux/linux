57b47ecf8f3910c9ab92864ac1314c0540d4c4a4
Enrico Weigelt, metux IT consult <info@metux.net>
2019-03-14 20:55:19 +0100

    drivers: tty: serial: amba-pl011: use devm_* functions
    Use the safer devm versions of memory mapping functions.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- drivers/tty/serial/amba-pl011.c
+++ drivers/tty/serial/amba-pl011.c
@@ -2098,7 +2098,7 @@ static const char *pl011_type(struct uart_port *port)
  */
 static void pl011_release_port(struct uart_port *port)
 {
-	release_mem_region(port->mapbase, SZ_4K);
+	devm_release_mem_region(port->dev, port->mapbase, SZ_4K);
 }
 
 /*
@@ -2106,7 +2106,10 @@ static void pl011_release_port(struct uart_port *port)
  */
 static int pl011_request_port(struct uart_port *port)
 {
-	return request_mem_region(port->mapbase, SZ_4K, "uart-pl011")
+	return devm_request_mem_region(port->dev,
+				       port->mapbase,
+				       SZ_4K,
+				       "uart-pl011")
 			!= NULL ? 0 : -EBUSY;
 }
 

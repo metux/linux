fced0aeb6499498a49178b02acc2128d11ae6be4
Enrico Weigelt, metux IT consult <info@metux.net>
2019-12-11 16:24:04 +0100

    drivers: net: usb: remove unneeded MODULE_VERSION() usage
    Remove MODULE_VERSION(), as it isn't needed at all: the only version
    making sense is the kernel version.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- drivers/net/usb/asix_devices.c
+++ drivers/net/usb/asix_devices.c
@@ -1383,7 +1383,6 @@ static struct usb_driver asix_driver = {
 module_usb_driver(asix_driver);
 
 MODULE_AUTHOR("David Hollis");
-MODULE_VERSION(DRIVER_VERSION);
 MODULE_DESCRIPTION("ASIX AX8817X based USB 2.0 Ethernet Devices");
 MODULE_LICENSE("GPL");
 

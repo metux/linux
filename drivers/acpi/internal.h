cd81da2e3be33e3f14a8412053b94b1a1366ee1f
Enrico Weigelt, metux IT consult <info@metux.net>
2020-11-12 17:14:31 +0100

    (submitted keep) drivers: acpi: add opt-out of apple specific property parsing
    Most x86 machines aren't Apple machines, especially VMs.
    Therefore allow opt-out, making the kernel a few KBs smaller,
    eg. for embedded or high-density VMs.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- drivers/acpi/internal.h
+++ drivers/acpi/internal.h
@@ -239,7 +239,7 @@ static inline void suspend_nvs_restore(void) {}
 void acpi_init_properties(struct acpi_device *adev);
 void acpi_free_properties(struct acpi_device *adev);
 
-#ifdef CONFIG_X86
+#ifdef CONFIG_ACPI_APPLE
 void acpi_extract_apple_properties(struct acpi_device *adev);
 #else
 static inline void acpi_extract_apple_properties(struct acpi_device *adev) {}
